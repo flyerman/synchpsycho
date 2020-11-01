d3cab1ebc26aba005be2d8fa0f1036eb2e8bbf5d
Florent Guiliani <florent@guiliani.fr>
2020-11-01 16:23:46 -0500

    initial commit

---
+++ Code.js
@@ -0,0 +1,21 @@
+function synchPsychos() {
+
+    // Retreive the names
+    var sheet = SpreadsheetApp.getActiveSheet();
+    var data = sheet.getDataRange().getValues();
+    var names = [];
+    for (var i = 0; i < data.length; i++) {
+        names.push(data[i][0]);
+    } 
+
+    // Set the names in the form
+    var form = FormApp.openById('1q0Cm5tntXxignq47RlgN2FKZ_uTRNLCQu4O8DQbAn14');
+    var formItems = form.getItems();
+    for (var i = 0, j = 0; i < formItems.length; i++) {
+        var formItem = formItems[i];
+        if (formItem.getType() == FormApp.ItemType.LIST) {
+            var listItem = formItem.asListItem();
+            listItem.setChoiceValues(names);
+        }
+    }
+}
